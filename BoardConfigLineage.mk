b8b6debc355e09109fb4f374479bfc6a796f278e
William Bellavance <flintman@flintmancomputers.com>
2022-06-28 09:53:58 -0400

    clean up
    Change-Id: I8e9201c27def3c05cf5103c4e5c308ae3bb2aee5

--- BoardConfigLineage.mk
+++
@@ -1,7 +0,0 @@
-#
-# Copyright (C) 2021 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-BUILD_BROKEN_DUP_RULES := true
--- device-ev.mk
+++ device-ev.mk
@@ -1,60 +1,140 @@
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor \
+    hardware.google.bluetooth.bt_channel_avoidance@1.0.vendor \
+    hardware.google.bluetooth.ccc@1.0.vendor \
+    hardware.google.bluetooth.sar@1.0.vendor \
+    hardware.google.bluetooth.sar@1.1.vendor
+
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
+# Codec2
+PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.0.vendor \
+    libavservices_minijail.vendor \
+    libavservices_minijail_vendor:32 \
+    libcodec2_hidl@1.0.vendor \
+    libcodec2_vndk.vendor \
+    libmedia_ecoservice.vendor \
+    libstagefright_bufferpool@2.0.1.vendor
+
+# Confirmation UI
+PRODUCT_PACKAGES += \
+    android.hardware.confirmationui@1.0-lib.trusty:64 \
+    android.hardware.confirmationui@1.0.vendor:64 \
+    libteeui_hal_support.vendor:64
+
+# HIDL
+PRODUCT_PACKAGES += \
+    libhwbinder.vendor
+
+# Identity credential
+PRODUCT_PACKAGES += \
+    android.hardware.identity-support-lib.vendor:64 \
+    android.hardware.identity_credential.xml
+
+# Nos
+PRODUCT_PACKAGES += \
+    libnos:64 \
+    libnosprotos:64 \
+    libnos_client_citadel:64 \
+    libnos_datagram:64 \
+    libnos_datagram_citadel:64 \
+    libnos_transport:64 \
+    nos_app_avb:64 \
+    nos_app_identity:64 \
+    nos_app_keymaster:64 \
+    nos_app_weaver:64
+
+# Tinycompress
+PRODUCT_PACKAGES += \
+    libtinycompress
+
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    libwifi-hal:64
+
+# Misc interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.authsecret@1.0.vendor \
+    android.hardware.biometrics.common-V1-ndk_platform.vendor:64 \
+    android.hardware.biometrics.fingerprint-V1-ndk_platform.vendor:64 \
+    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
+    android.hardware.input.classifier@1.0.vendor:64 \
+    android.hardware.input.common@1.0.vendor:64 \
+    android.hardware.keymaster@3.0.vendor \
+    android.hardware.keymaster@4.0.vendor \
+    android.hardware.keymaster@4.1.vendor \
+    android.hardware.neuralnetworks-V1-ndk_platform.vendor \
+    android.hardware.oemlock@1.0.vendor:64 \
+    android.hardware.power@1.0.vendor:64 \
+    android.hardware.power@1.1.vendor:64 \
+    android.hardware.power@1.2.vendor:64 \
+    android.hardware.radio.config@1.0.vendor \
+    android.hardware.radio.config@1.1.vendor \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.radio@1.2.vendor \
+    android.hardware.radio@1.3.vendor \
+    android.hardware.radio@1.4.vendor \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio@1.6.vendor \
+    android.hardware.sensors@1.0.vendor \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor \
+    android.hardware.sensors@2.0.vendor \
+    android.hardware.sensors@2.1.vendor \
+    android.hardware.tetheroffload.config@1.0.vendor:64 \
+    android.hardware.tetheroffload.control@1.0.vendor:64 \
+    android.hardware.tetheroffload.control@1.1.vendor:64 \
+    android.hardware.weaver@1.0.vendor:64 \
+    android.hardware.wifi@1.1.vendor:64 \
+    android.hardware.wifi@1.2.vendor:64 \
+    android.hardware.wifi@1.3.vendor:64 \
+    android.hardware.wifi@1.4.vendor:64 \
+    android.hardware.neuralnetworks@1.3.vendor:64 \
+    libaudioroutev2.vendor \
+    libexynosutils \
+    libexynosv4l2 \
+    libhidltransport.vendor \
+    libsfplugin_ccodec_utils.vendor \
+    libavservices_minijail_vendor \
+    libcppbor.vendor \
+    libkeymaster4support.vendor \
+    libnetfilter_conntrack \
+    libnfnetlink \
+    libsensorndkbridge \
+    libtrusty_metrics \
+    pixelpowerstats_provider_aidl_interface-cpp.vendor \
+    sensors.dynamic_sensor_hal
+
 # Libraries required for vendor
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor:64 \
-    android.hardware.authsecret@1.0.vendor:64 \
-    android.hardware.biometrics.fingerprint-V1-ndk_platform.vendor:64 \
-    android.hardware.bluetooth@1.1.vendor:64 \
-    android.hardware.confirmationui@1.0.vendor:64 \
-    android.hardware.confirmationui@1.0-lib.trusty:64 \
     android.hardware.drm@1.4.vendor:64 \
-    android.hardware.keymaster@4.1.vendor:64 \
-    android.hardware.oemlock@1.0.vendor:64 \
     android.hardware.identity_credential.xml \
     android.hardware.identity-support-lib.vendor:64 \
     android.hardware.input.classifier@1.0.vendor:64 \
-    android.hardware.neuralnetworks-V1-ndk_platform.vendor:64 \
-    android.hardware.neuralnetworks@1.3.vendor:64 \
-    android.hardware.power@1.2.vendor:64 \
-    android.hardware.radio@1.6.vendor \
-    android.hardware.radio.config@1.2.vendor \
-    android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.sensors@2.0-ScopedWakelock.vendor \
-    android.hardware.sensors@2.1.vendor \
-    android.hardware.tetheroffload.config@1.0.vendor:64 \
-    android.hardware.tetheroffload.control@1.1.vendor:64 \
-    android.hardware.weaver@1.0.vendor:64 \
-    android.hardware.wifi@1.5.vendor:64 \
     hardware.google.bluetooth.bt_channel_avoidance@1.0.vendor:64 \
     hardware.google.bluetooth.ccc@1.0.vendor:64 \
     hardware.google.bluetooth.sar@1.1.vendor:64 \
-    pixelpowerstats_provider_aidl_interface-cpp.vendor:64 \
-    libaudioroutev2.vendor \
-    libavservices_minijail_vendor:64 \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
-    libcppbor.vendor:64 \
-    libGralloc4Wrapper \
-    libexynosv4l2 \
-    libexynosutils \
     libhwbinder.vendor \
     libmedia_ecoservice.vendor \
-    libkeymaster4support.vendor:64 \
-    libnetfilter_conntrack:64 \
-    libnfnetlink:64 \
     libnos:64 \
     libnos_client_citadel:64 \
     libnos_datagram:64 \
     libnos_datagram_citadel:64 \
-    libsensorndkbridge \
-    libsfplugin_ccodec_utils.vendor \
     libtinycompress \
-    libtrusty_metrics:64 \
     libwifi-hal:64 \
     nos_app_avb:64 \
     nos_app_identity:64 \
     nos_app_keymaster:64 \
     nos_app_weaver:64 \
-    sensors.dynamic_sensor_hal \
     vendor.google.google_battery@1.1.vendor:64 \
     vendor.google.wireless_charger@1.3.vendor:64
 
